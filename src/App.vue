d79cc7ba0e31cce78396213aba4073c6c4898b06
Jorge Enrrique Fonseca Murillo <jorge.fonseca22f@gmail.com>
2025-03-23 10:20:42 -0500

    Segundo commit, se le agrega funcionalidad del carrito de compras

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,58 @@
 <script setup>
+    import {ref, reactive, onMounted} from 'vue'
+    import {db} from './data/guitarras'
+    import Guitarra from './components/Guitarra.vue'
+    import Header from './components/Header.vue'
+    import Footer from './components/Footer.vue'
 
+    const guitarras = ref([])
+    const carrito = ref([])
+
+    onMounted(() => {
+        guitarras.value = db
+    })
+
+    const agregarCarrito = (guitarra) =>{
+        const existeCarrito = carrito.value.findIndex(producto => producto.id === guitarra.id)
+
+        if(existeCarrito >= 0){
+            carrito.value[existeCarrito].cantidad++
+        }else{
+            guitarra.cantidad = 1
+            carrito.value.push(guitarra)
+        }
+        
+    }
+
+    const decrementarCantidad = () => {
+
+    }
+
+    const incrementarCantidad = () => {
+
+    }
+    
 </script>
 
 <template>
 
+    <Header 
+        :carrito="carrito"
+        @decrementar-cantidad="decrementarCantidad"
+        @incrementar-cantidad="incrementarCantidad"
+    />
+  
+    <main class="container-xl mt-5">
+        <h2 class="text-center">Nuestra Colección</h2>
+
+        <div class="row mt-5">
+            <Guitarra
+                v-for="guitarra in guitarras"
+                :guitarra="guitarra"
+                @agregar-carrito="agregarCarrito"
+            />
+        </div>
+    </main>
+
+    <Footer />
 </template>
-
-<style scoped>
-
-</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+    <footer class="bg-dark mt-5 py-5">
+        <div class="container-xl">
+            <p class="text-white text-center fs-4 mt-4 m-md-0">Bardo Guitars - Todos los derechos Reservados</p>
+        </div>
+    </footer>
+</template>
+
